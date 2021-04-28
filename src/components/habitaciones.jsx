ed9ef6791afdb3462ddb57e63823c3e518b3708b
Alejandro <alejandro.becerra.pro@gmail.com>
2021-04-28 14:46:49 -0500

    haciendo la consulta de la habitacion segun el slug, y la creacion de paginas dinamicas

--- src/components/habitaciones.jsx
+++ src/components/habitaciones.jsx
@@ -1,6 +1,21 @@
 import React from 'react';
+import {graphql, useStaticQuery} from 'gatsby';
 
-const HabitacionTemplate = () => {
+export const query = graphql`
+query($slug: String!){
+	allDatoCmsHabitacion(filter:{slug:{eq: $slug}}) {
+        nodes{
+                titulo
+                contenido
+            imagen{
+                gatsbyImageData
+            }
+        }
+	}
+}
+`;
+const HabitacionTemplate = ({data}) => {
+    console.log(data)
     return ( 
         <p>habitacion.jsx</p>
      );
